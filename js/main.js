3ce8d680fd18decef7153b467c4b92f73c31fb6b
Roman Gulevskiy <roman.gulevskiy@gmail.com>
2023-03-07 12:59:49 +0500

    fix mod. 4 les. 2

--- js/main.js
+++ js/main.js
@@ -10,24 +10,24 @@
   console.log(itemQuantity * itemPrice);
 }
 
-// {
-//   const itemName = prompt('Введите наименование товара:');
-//   const itemQuantity = prompt('Введите количество товара:');
-//   const itemCategory = prompt('Введите категорию товара:');
-//   const itemPrice = prompt('Введите цену товара:');
-//
-//   console.log(typeof itemName);
-//   console.log(typeof itemQuantity);
-//   console.log(typeof itemCategory);
-//   console.log(typeof itemPrice);
-//
-//   if (Number.isNaN(+itemQuantity)) {
-//     console.log('Вы ввели некорректное количество.');
-//   }
-//
-//   if (Number.isNaN(+itemPrice)) {
-//     console.log('Вы ввели некорректную цену.');
-//   }
-//
-//   console.log(`В наличии ${itemQuantity} единиц(-ы) товара ${itemName} на общую сумму ${itemQuantity * itemPrice} рублей.`);
-// }
+{
+  const itemName = prompt('Введите наименование товара:');
+  const itemQuantity = +prompt('Введите количество товара:');
+  const itemCategory = prompt('Введите категорию товара:');
+  const itemPrice = +prompt('Введите цену товара:');
+
+  console.log(typeof itemName);
+  console.log(typeof itemQuantity);
+  console.log(typeof itemCategory);
+  console.log(typeof itemPrice);
+
+  if (Number.isNaN(itemQuantity)) {
+    console.log('Вы ввели некорректное количество.');
+  }
+
+  if (Number.isNaN(itemPrice)) {
+    console.log('Вы ввели некорректную цену.');
+  }
+
+  console.log(`В наличии ${itemQuantity} единиц(-ы) товара ${itemName} на общую сумму ${itemQuantity * itemPrice} рублей.`);
+}
