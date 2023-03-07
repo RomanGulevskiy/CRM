e112d170f4cf5495363d8d89f479594360f206a4
Roman Gulevskiy <roman.gulevskiy@gmail.com>
2023-03-07 22:42:23 +0500

    fix mod. 4 les. 3

--- js/tasksHard_2.js
+++ js/tasksHard_2.js
@@ -1,22 +1,23 @@
 'use strict';
 
 const income = +prompt('Введите сумму вашего дохода.');
-let tax;
+let totalTax = 0;
 
 if (!Number.isNaN(income)) {
-  switch (true) {
-    case (income < 15000):
-      tax = income * 0.13;
-      console.log(`Сумма вашего налога состовляет ${tax} рублей.`);
-      break;
-    case (income >= 15000 && income < 50000):
-      tax = (income - 14999) * 0.2 + 14999 * 0.13;
-      console.log(`Сумма вашего налога состовляет ${tax} рублей.`);
-      break;
-    case (income >= 50000):
-      tax = (income - 49999) * 0.3 + (income - 14999) * 0.2 + 14999 * 0.13;
-      console.log(`Сумма вашего налога состовляет ${tax} рублей.`);
+  if (income < 15000) {
+    totalTax += income * 0.13;
   }
+
+  if (income >= 15000 && income < 50000) {
+    totalTax += (income - 14999) * 0.2;
+  }
+
+  if (income >= 50000) {
+    totalTax += (income - 49999) * 0.3;
+  }
+
+  console.log(`Сумма вашего налога состовляет ${totalTax} рублей.`);
+
 } else {
   console.log('Вы ввели некорректные данные.');
 }
